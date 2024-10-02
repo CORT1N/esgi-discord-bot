ba7c60b23ddaba75883616c65dd09975e442a524
cort1n <lbonnairepro@gmail.com>
2024-10-02 18:08:59 +0000

    added a poc for discord.py bot

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM python:3.9.20-alpine3.20
 
-WORKDIR /code
+WORKDIR /usr/src/app
 
 # vulnerability fix
 RUN python3 -m pip install --upgrade pip==23.3 setuptools==70.0.0
 
 # discord.py installation
 RUN python3 -m pip install -U discord.py
+
+# copying bot scripts
+COPY ./bot .
+
+# launching the bot
+CMD ["python3", "-u", "main.py"]
---
+++ bot/main.py
@@ -0,0 +1,25 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import os
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} logged in.')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+bot.run(DISCORD_TOKEN)
