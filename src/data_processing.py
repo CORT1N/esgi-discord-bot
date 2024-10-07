389da8a7c12b34a4b34c8d503f0104f3f11cab03
cort1n <lbonnairepro@gmail.com>
2024-10-07 18:40:18 +0000

    added env variable for subjects file path

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,9 @@
 from openpyxl import load_workbook
+import os
 
 def get_subjects_by_year():
-    wb =  load_workbook(filename='/tmp/imports/subjects_by_year.xlsx', read_only=True)
+    WB_PATH = os.getenv('SUBJECTS_FILE_PATH')
+    wb =  load_workbook(filename=WB_PATH, read_only=True)
     subjects_by_year = {}
     for sheet in wb:
         year_of_study = sheet.title
@@ -11,10 +13,10 @@ def get_subjects_by_year():
             if subject:
                 subjects.append(subject)
         subjects_by_year[year_of_study] = subjects
-    # # Debug 
-    # for year_of_study, subjects in subjects_by_year.items():
-    #     print(f"Year of study: {year_of_study}")
-    #     print("Subjects:")
-    #     for subject in subjects:
-    #         print(f"- {subject}")
+    # Debug 
+    for year_of_study, subjects in subjects_by_year.items():
+        print(f"Year of study: {year_of_study}")
+        print("Subjects:")
+        for subject in subjects:
+            print(f"- {subject}")
     return subjects_by_year
